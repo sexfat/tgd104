9363b96e4af9ac5821bee2d52f28e489d466ba09
sexfat <bryant6831@gmail.com>
2023-03-28 14:01:32 +0800

    add

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const a = (x , y) => x * y;
+console.log(a(50, 10));
+
+import $ from 'jquery';
+import { gsap } from "gsap";
+
+
+$('body').css('background-color' , '#f20');
+
+gsap.to('.box' , {
+    x: 400,
+    y: 500,
+    width: '300px',
+    duration: 1,
+    rotation :  360,
+    delay: .3,
+    repeat: -1,
+    yoyo : true,
+    repeatDelay: .3
+
+})
+
+
+
